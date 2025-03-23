4b0b63e5e3d2514e227e9f2ccc3c93003d8e6036
VaclavHrbek <st43190@student.upce.cz>
2025-03-23 21:10:04 +0000

    Replace Nuxt components in app.vue and add index.vue with a new layout and button

---
+++ dashboard-app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <UApp>
+        <div class="flex flex-col items-center justify-center h-screen">
+            <UButton :ui="{
+                base: 'p-20 text-[50px]',
+            }">To-Do</UButton>
+        </div>
+    </UApp>
+</template>
