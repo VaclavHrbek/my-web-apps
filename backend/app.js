6ec31e2364739f3c6e6a7f542986117584482835
VaclavHrbek <st43190@student.upce.cz>
2025-03-31 23:06:38 +0200

    Refactor backend: replace database connection logic with new Client class and update app structure

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,21 @@
-const client = require('./connect_database');
+import { Client } from './database.js';
+import express from 'express';
 
-
-const express = require('express')
 const app = express()
 const port = 3000
 
-const get_one_todo_item = async () => {
-    try {
-        const database = client.db('to-do');
-        const items = database.collection('to-do-items');
-        const item = await items.findOne();
-        return item;
-    } finally {
-        // await client.close();
-    }
-}
+const client = new Client();
+
 
 app.get('/', async (req, res) => {
     try {
-        const item = await get_one_todo_item();
+        const item = await client.get_all_todo_items();
         res.send(item);
     } catch (error) {
         res.status(500).send(error.message);
     }
 });
 
-const cb0 = function (req, res, next) {
-    console.log('CB0')
-    next()
-  }
-  
-  const cb1 = function (req, res, next) {
-    console.log('CB1')
-    next()
-  }
-  
-  const cb2 = function (req, res) {
-    res.send('Hello from C!')
-  }
-  
-  app.get('/example/c', [cb0, cb1, cb2])
-  
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Listening on port ${port}`)
 })
