f43898d83643cf097de666172f906f092e654797
VaclavHrbek <st43190@student.upce.cz>
2025-03-31 23:06:15 +0200

    Refactor database connection: replace connect_database.js with a new database.js class for improved connection management and data retrieval

---
+++ backend/database.js
@@ -0,0 +1,35 @@
+import { MongoClient } from "mongodb";
+// Replace the uri string with your connection string.
+const uri = "mongodb://root:example@database:27017";
+
+export class Client {
+    constructor(uri) {
+        this.isConnected = false; 
+        this.connect();
+    }
+
+    async connect() {
+        try {
+            this.client = new MongoClient(uri); 
+            this.isConnected = true;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async get_all_todo_items() {
+        if (!this.isConnected) {
+            await this.connect();
+        }
+        const database = this.client.db("to-do");
+        const collection = database.collection("to-do-items");
+        const items = await collection.find({}).toArray();
+        return items;
+    }
+
+    disconnect() {
+        this.client.close();
+    }
+
+    
+}
